5bd3dde186baabf0c5b06236d70a8b7f5c719f17
T. Andrew Manning <manninga@illinois.edu>
2025-12-05 14:46:55 -0600

    Initial data file manifest script and docs

--- docs/developer.md
+++ docs/developer.md
@@ -8,6 +8,50 @@ After cloning the source code repo, install the data files stored in Git LFS:
 git lfs pull
 ```
 
+### Initial data S3 upload and manifest generation
+
+Prepare the S3 bucket for storing the version-controlled DASH initial data objects:
+
+```bash
+# S3 endpoint configuration using MinIO CLI "mc"
+#
+$ mc alias ls js-blast
+js-blast
+  URL       : https://js2.jetstream-cloud.org:8001
+  AccessKey : ******
+  SecretKey : ******
+  API       : s3v4
+  Path      : auto
+
+# Enable bucket versioning
+#
+mc version enable js-blast/dash
+mc version info js-blast/dash
+
+# Upload data files from local Git LFS clone
+#
+mc mirror ${GIT_CLONE_PATH}/data/ js-blast/dash/init_data/
+
+# Set the access credential env vars
+export S3_ACCESS_KEY_ID_INIT="****"
+export S3_SECRET_ACCESS_KEY_INIT="****"
+
+# Install dependencies in a virtual environment
+cd ${GIT_CLONE_PATH}/scripts
+python -m venv .venv
+source .venv/bin/activate
+pip install minio
+
+# Run the generator
+#
+$ python generate_manifest.py
+
+Creating a MinIO client...
+Listing objects in "s3://js2.jetstream-cloud.org:8001/dash/init_data/"...
+Writing manifest file "../data/dash-data.json"...
+Manifest generation complete.
+```
+
 ## Run DASH locally in Docker Compose
 
 Build and launch the application using:
---
+++ scripts/generate_manifest.py
@@ -0,0 +1,49 @@
+from minio import Minio
+import os
+from pathlib import Path
+import json
+
+
+'''
+Generate a JSON-formatted manifest of the latest versions of S3 objects.
+
+Set S3_ACCESS_KEY_ID_INIT and S3_SECRET_ACCESS_KEY_INIT in the env vars.
+'''
+endpoint = os.getenv("S3_ENDPOINT_URL_INIT", 'js2.jetstream-cloud.org:8001')
+bucket = os.getenv("S3_BUCKET_INIT", 'dash')
+base_path = os.getenv("S3_BASE_PATH_INIT", "init_data/")
+print('Creating a MinIO client...')
+client = Minio(
+    endpoint=endpoint,
+    access_key=os.getenv('S3_ACCESS_KEY_ID_INIT'),
+    secret_key=os.getenv('S3_SECRET_ACCESS_KEY_INIT'),
+    secure=True,
+    # region='',
+)
+
+print(f'Listing objects in "s3://{os.path.join(endpoint, bucket, base_path)}"...')
+objects = client.list_objects(
+    bucket_name=bucket,
+    prefix=base_path,
+    include_version=True,
+    recursive=True,
+)
+
+# Collect metadata for each file
+file_info = []
+for obj in [obj for obj in objects]:
+    info = {
+        'path': obj.object_name.replace(base_path, ''),
+        'version_id': obj.version_id,
+        'etag': obj.etag,
+        'size': obj.size,
+    }
+    if obj.is_latest.lower() == "true":
+        file_info.append(info)
+
+manifest_filepath = os.path.join(Path(__file__).resolve().parent.parent, 'dash-data.json')
+print(f'Writing manifest file "{manifest_filepath}"...')
+with open(manifest_filepath, 'w') as fh:
+    json.dump(file_info, fh, indent=2)
+
+print('Manifest generation complete.')
